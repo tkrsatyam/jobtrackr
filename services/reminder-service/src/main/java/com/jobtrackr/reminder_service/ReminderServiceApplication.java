db996d8009893f4992f0b5f7b8719fe8c73da797
satyamk7 <satyamk.1107@gmail.com>
2026-05-28 21:31:45 +0530

    feat: Initialize microservices for contact, document, eureka, notification, reminder, and user services
    - Added ContactServiceApplication with basic Spring Boot setup.
    - Created application.properties for contact service.
    - Implemented ContactServiceApplicationTests for context loading verification.
    
    - Added DocumentServiceApplication with basic Spring Boot setup.
    - Created application.properties for document service.
    - Implemented DocumentServiceApplicationTests for context loading verification.
    
    - Added EurekaServerApplication with basic Spring Boot setup.
    - Created application.properties for eureka server.
    - Implemented EurekaServerApplicationTests for context loading verification.
    
    - Added NotificationServiceApplication with basic Spring Boot setup.
    - Created application.properties for notification service.
    - Implemented NotificationServiceApplicationTests for context loading verification.
    
    - Added ReminderServiceApplication with basic Spring Boot setup.
    - Created application.properties for reminder service.
    - Implemented ReminderServiceApplicationTests for context loading verification.
    
    - Added UserServiceApplication with basic Spring Boot setup.
    - Created application.properties for user service.
    - Implemented UserServiceApplicationTests for context loading verification.
    
    - Created pom.xml and .gitignore files for each service with necessary dependencies and configurations.

---
+++ services/reminder-service/src/main/java/com/jobtrackr/reminder_service/ReminderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jobtrackr.reminder_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderServiceApplication.class, args);
+	}
+
+}
